7e73819c351d32a5381c3bf561636f7d665f44a0
alexf <afrings712@gmail.com>
2017-08-22 14:59:43 -0400

    New Java Class added to model questions.

--- app/src/main/java/com/fringsproduction/android/geoquiz/QuizActivity.java
+++ app/src/main/java/com/fringsproduction/android/geoquiz/QuizActivity.java
@@ -4,22 +4,49 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class QuizActivity extends AppCompatActivity {
 
     private Button mTrueButton;
     private Button mFalseButton;
+    private Button mNextButton;
+    private TextView mQuestionTextView;
+
+    //Create array of Questions from strings.xml, which takes the resId in constructor
+    private Question[] mQuestionBank = new Question[] {
+        new Question(R.string.question_australia, true),
+        new Question(R.string.question_oceans, true),
+        new Question(R.string.question_mideast, false),
+        new Question(R.string.question_africa, false),
+        new Question(R.string.question_americas, true),
+        new Question(R.string.question_asia, true),
+    };
+
+    private int mCurrentIndex = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_quiz);
 
+        //Set up textview to display current index (mCurrentIndex) question string
+        mQuestionTextView = (TextView) findViewById(R.id.question_text_view);
+
+        //Gets question id from Question.java using getter
+        int question = mQuestionBank[mCurrentIndex].getTextResId();
+
+        //Set text based on question resource id
+        mQuestionTextView.setText(question);
+
+
         mTrueButton = (Button) findViewById(R.id.true_button);
+        //Uses anonymous class to override onClick method
         mTrueButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v){
+                //Use QuizActivity.this because this is View.OnClickListener in this class
                 Toast.makeText(QuizActivity.this, R.string.correct_toast, Toast.LENGTH_SHORT).show();
             }
         });
